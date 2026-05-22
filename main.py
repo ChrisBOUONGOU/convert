db24705049ba46a63be6fd1c5dd7d35c149925b3
ChrisBOUONGOU <chrisbouk@outlook.fr>
2026-05-22 08:32:56 -0400

    convert to Miles, Fahrenheit and Pound

---
+++ main.py
@@ -0,0 +1,10 @@
+def convertMiles(km):
+    return km/1.609
+def convertFahrenheit(celsius):
+    return ((celsius*1.8)+32)
+def convertPound(kg):
+    return kg*2.205
+
+print(convertMiles(1))
+print(convertFahrenheit(0))
+print(convertPound(3))
