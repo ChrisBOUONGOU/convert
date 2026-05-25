dc51ece9c6998c3ca497694d3db8f254a2100675
ChrisBOUONGOU <chrisbouk@outlook.fr>
2026-05-25 14:32:30 -0400

    conver money

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def convertMiles(km):
-    return km/1.609
-def convertFahrenheit(celsius):
-    return ((celsius*1.8)+32)
-def convertPound(kg):
-    return kg*2.205
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
 
-print(convertMiles(1))
-print(convertFahrenheit(0))
-print(convertPound(3))
+def convertCadeur(cad):
+    return round(c.convert('CAD' ,'EUR', cad), 2)
+
+print(convertCadeur(160), "Euros")
+
+
