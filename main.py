e34fa4f35a6cf0ddae0cfe6aeb1e3bc94807bb19
ChrisBOUONGOU <chrisbouk@outlook.fr>
2026-05-25 14:35:58 -0400

    convert money(CAD -> USD)

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ c = CurrencyRates()
 def convertCadeur(cad):
     return round(c.convert('CAD' ,'EUR', cad), 2)
 
+def convertCadusd(cad):
+    return round(c.convert('CAD' ,'USD', cad), 2)
+
+
 print(convertCadeur(160), "Euros")
+print(convertCadeur(160), "US dollars")
 
 
